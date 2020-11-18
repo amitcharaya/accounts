3c36ff97cc93648e70301c7f595df0fef3c765f5
amit <amit@learn4test.com>
2020-11-18 19:28:38 +0530

    style cust id

--- heads/forms.py
+++ heads/forms.py
@@ -7,3 +7,7 @@ class CustIdForm(forms.ModelForm):
     class Meta:
         model=CustomerID
         fields='__all__'
+        widgets={
+            'name': forms.TimeInput(attrs={'class':'form-control'}),
+            'father_name': forms.TimeInput(attrs={'class': 'form-control'})
+        }
